5210217651cbfd9631bc06e1178fa40fc72b704d
horimislime <horimislime@gmail.com>
2019-02-09 16:48:09 +0900

    Save location.href to clipboard by clicking menubar icon

--- MenubarExample Extension/SafariExtensionHandler.swift
+++ MenubarExample Extension/SafariExtensionHandler.swift
@@ -11,24 +11,39 @@ import SafariServices
 class SafariExtensionHandler: SFSafariExtensionHandler {
     
     override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
-        // This method will be called when a content script provided by your extension calls safari.extension.dispatchMessage("message").
-        page.getPropertiesWithCompletionHandler { properties in
-            NSLog("The extension received a message (\(messageName)) from a script injected into (\(String(describing: properties?.url))) with userInfo (\(userInfo ?? [:]))")
+        
+        // WHen injected script calls safari.extension.dispatchMessage, the message will come here
+        NSLog("Message received: %@", messageName);
+        
+        guard let href = userInfo?["href"] as? String else {
+            NSLog("Invalid userInfo");
+            return
         }
+        NSPasteboard.general.clearContents()
+        NSPasteboard.general.setString(href, forType: .string)
+        NSLog("Saved '%@' to clipboard", href)
     }
     
     override func toolbarItemClicked(in window: SFSafariWindow) {
-        // This method will be called when your toolbar item is clicked.
-        NSLog("The extension's toolbar item was clicked")
+        NSLog("The extension's toolbar item was clicked!")
+        
+        // Request injected script a message to send location.href
+        window.getActiveTab { currentTab in
+            currentTab!.getActivePage { currentPage in
+                currentPage!.dispatchMessageToScript(withName: "getHref", userInfo: nil)
+            }
+        }
+        
+        // You have an option to get the current page's URL without using injected scripts
+        //        window.getActiveTab { currentTab in
+        //            currentTab!.getActivePage { currentPage in
+        //                currentPage!.getPropertiesWithCompletionHandler { properties in
+        //                    let urlString = properties!.url!.absoluteString
+        //                    NSPasteboard.general.clearContents()
+        //                    NSPasteboard.general.setString(urlString, forType: .string)
+        //                    NSLog("Saved '%@' to clipboard", urlString)
+        //                }
+        //            }
+        //        }
     }
-    
-    override func validateToolbarItem(in window: SFSafariWindow, validationHandler: @escaping ((Bool, String) -> Void)) {
-        // This is called when Safari's state changed in some way that would require the extension's toolbar item to be validated again.
-        validationHandler(true, "")
-    }
-    
-    override func popoverViewController() -> SFSafariExtensionViewController {
-        return SafariExtensionViewController.shared
-    }
-
 }
--- MenubarExample Extension/script.js
+++ MenubarExample Extension/script.js
@@ -1,3 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    safari.extension.dispatchMessage("Hello World!");
+safari.self.addEventListener("message", function(event) {
+  console.log("event received");
+  safari.extension.dispatchMessage("sendHref", { "href": location.href });
 });
